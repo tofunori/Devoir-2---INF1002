51681aa46b3014d3443cd5fcd07d1e4ece700880
Pengou2 <pengouange@gmail.com>
2025-04-01 21:08:00 -0400

    Update Disponibilite.java

--- Devoir4/src/src/Disponibilite.java
+++ Devoir4/src/src/Disponibilite.java
@@ -1,17 +1,4 @@
-/*                            Devoir n° 2
-                           INF 1002 (java)
- *                       Session d’automne 2022
- *         
- *                       travail fait par :
- *                       
-             ---Fouodji Bokou, Teddy Pollar Teddy.Pollar.Fouodji.Bokou@uqtr.ca
-
-             ---Faye, Youssouf Youssouf.Faye@uqtr.ca
-
-             ---Fosnea, Vadim Vadim.Fosnea@uqtr.ca
-
- *            ---Ewetola, Firaas Esso-Ninam Firaas.Esso-Ninam.Ewetola@uqtr.ca
- *            
+/*                    
  *            ##Description##
 
  * La classe Disponiblite est une classe  qui permet de gerer la disponibilite d'un 
@@ -21,7 +8,7 @@
  * -Deux variables(jours de type de l'enum Jours et l'autre de type boolean
  *  - Un Getter(isDisponible)
  *  -Un Setter(setDisponible)
-	-  une m�thode tostring pour l'affichage
+	-  une méthode tostring pour l'affichage
 	
  *  
 
